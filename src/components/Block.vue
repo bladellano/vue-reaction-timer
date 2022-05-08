420849d51276757cb0d791953c5ad40048afbb6d
Caio <bladellano@gmail.com>
2022-05-07 21:39:04 -0300

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTimer: 0,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  updated() {
+    console.log("Component updated");
+  },
+  unmounted() {
+    console.log("Component unmounted");
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTimer += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit('end',this.reactionTimer)
+    },
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
